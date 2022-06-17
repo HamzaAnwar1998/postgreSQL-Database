c19477ef05d0c88e0e91299883f4fb814afeeffc
HamzaAnwar1998 <anwarhamza919@gmail.com>
2022-06-17 15:14:20 +0500

    add more queries

--- schema.sql
+++ schema.sql
@@ -8,3 +8,17 @@ CREATE TABLE animals(
   neutered BOOLEAN,
   weight_kg decimal
 );
+
+create table species (
+id   bigserial PRIMARY KEY,
+name varchar(20)
+);
+
+ALTER TABLE animals
+ADD species VARCHAR(20);
+
+ALTER TABLE animals
+drop COLUMN species;
+
+ALTER TABLE animals
+ADD species_id BIGINT REFERENCES species(id);
